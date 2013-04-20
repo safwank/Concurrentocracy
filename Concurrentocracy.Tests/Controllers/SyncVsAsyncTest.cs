c7fe2167d8efa42fd9156b9ad048bc8e133e725b
Safwan Kamarrudin <shaihulud@alumni.cmu.edu>
2013-04-20 20:21:52 +1000

    Add local async test

--- Concurrentocracy.Tests/Controllers/SyncVsAsyncTest.cs
+++ Concurrentocracy.Tests/Controllers/SyncVsAsyncTest.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using RestSharp;
 
 namespace Concurrentocracy.Tests.Controllers
 {
@@ -13,19 +14,10 @@ namespace Concurrentocracy.Tests.Controllers
     public class SyncVsAsyncTest
     {
         [TestMethod]
-        public void Async_Should_Be_Faster_Than_Sync1()
+        public void Remote_Async_Should_Be_Faster()
         {
             var asyncTiming = HitAsyncAndGetTimeElapsed();
             var syncTiming = HitSyncAndGetTimeElapsed();
-
-            asyncTiming.Should().BeLessThan(syncTiming);
-        }
-
-        [TestMethod]
-        public void Async_Should_Be_Faster_Than_Sync2()
-        {
-            var syncTiming = HitSyncAndGetTimeElapsed();
-            var asyncTiming = HitAsyncAndGetTimeElapsed();
 
             asyncTiming.Should().BeLessThan(syncTiming);
         }
@@ -50,13 +42,13 @@ namespace Concurrentocracy.Tests.Controllers
         }
 
         [TestMethod]
-        public void Run_Async_Only()
+        public void Run_Remote_Async_Only()
         {
             QueueHits(HitAsync);
         }
 
         [TestMethod]
-        public void Run_Sync_Only()
+        public void Run_Remote_Sync_Only()
         {
             QueueHits(HitSync);
         }
@@ -65,10 +57,10 @@ namespace Concurrentocracy.Tests.Controllers
         {
             var tasks = new List<Task>();
             Enumerable.Range(1, 1000).ToList().ForEach(i =>
-                                                         {
-                                                             var task = Task.Factory.StartNew(hitAction);
-                                                             tasks.Add(task);
-                                                         });
+                                                           {
+                                                               var task = Task.Factory.StartNew(hitAction);
+                                                               tasks.Add(task);
+                                                           });
             Task.WaitAll(tasks.ToArray());
         }
 
@@ -87,6 +79,39 @@ namespace Concurrentocracy.Tests.Controllers
             var restClient = new RestClient(url);
             var request = new RestRequest(Method.GET);
             restClient.Get(request);
+        }
+
+        [TestMethod]
+        public void Local_Async_Should_Be_Faster()
+        {
+            var iterations = Enumerable.Range(1, 10000).ToList();
+
+            var tasks = new List<Task>();
+            var stopWatch = Stopwatch.StartNew();
+            iterations.ForEach(i =>
+                                   {
+                                       var task = Task.Delay(1);
+                                       tasks.Add(task);
+                                   });
+            Task.WaitAll(tasks.ToArray());
+            stopWatch.Stop();
+            var asyncElapsed = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine(asyncElapsed);
+
+            stopWatch = Stopwatch.StartNew();
+            iterations.ForEach(i => Thread.Sleep(1));
+            stopWatch.Stop();
+            var syncElapsed = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine(syncElapsed);
+
+            stopWatch = Stopwatch.StartNew();
+            Parallel.ForEach(iterations, i => Thread.Sleep(1));
+            stopWatch.Stop();
+            var parallelElapsed = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine(parallelElapsed);
+
+            asyncElapsed.Should().BeLessThan(syncElapsed);
+            asyncElapsed.Should().BeLessThan(parallelElapsed);
         }
     }
 }
